835ca910055eecc669fdd61a54571d13ad3f299f
snrhokage <dmitry.karsakov.2003@gmail.com>
2025-12-21 15:03:46 +0500

    test

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('WOW');
+import { app, BrowserWindow } from 'electron';
+
+const createWindow = () => {
+  win = new BrowserWindow();
+  win.loadURL('https://google.com');
+  
+};
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+    win = null;
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+app.whenReady().then(createWindow);
